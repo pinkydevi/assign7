e88622565c5f88b53c40660954f57c9c32c5720c
Pinky Devi <pinkydevi456u@gmail.com>
2023-11-11 23:47:37 +0600

    assign7

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?
+
+// app/Http/Controllers/UserController.php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function sayHello()
+    {
+        return 'Hello, Laravel!';
+    }
+}
+
+?>
